8f60f3f041129066baff3d5d6b21939099e8a380
deanlauvina7 <deanlauvina7@gmail.com>
2020-09-21 06:00:31 -0500

    Scrolling to elements

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,4 @@
+// FOR THE STICKY NAVIGATION
 $(document).ready(function () {
   $('.js--section-features').waypoint(function (direction) {
     if (direction == "down") {
@@ -6,5 +7,34 @@ $(document).ready(function () {
       $('nav').removeClass('sticky');
     }
     offset: '60px;'
-  })
-})
+  });
+
+  // SCROLL ON BUTTONS
+  $('.js--scroll-to-plans').click(function () {
+    $('html, body').animate({
+      scrollTop: $('.js--section-plans').offset().top
+    }, 1000);
+  });
+
+  $('.js--scroll-to-start').click(function () {
+    $('html, body').animate({
+      scrollTop: $('.js--section-features').offset().top
+    }, 1000);
+  });
+
+  // NAVIGATION SCROLL
+  $(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+});
